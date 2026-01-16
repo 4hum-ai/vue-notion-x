20023111ea4a7dc82550f3f717bb2900de90af86
slogvo <volonglqd@gmail.com>
2026-01-17 00:15:44 +0700

    fix: remove outdated vue/macros-global reference to fix typecheck failure
    - Remove outdated reference in env.d.ts
    - Fix empty object types to satisfy lint rules

--- packages/vue-notion-x/env.d.ts
+++ packages/vue-notion-x/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
-/// <reference types="vue/macros-global" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<object, object, any>
   export default component
 }
